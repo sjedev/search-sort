cdfa8ced478547c1388dd904ee73c133433c831c
Sebastien <sebastien.jensen@proton.me>
2023-01-30 14:05:40 +0000

    beep beep

--- linear.js
+++ linear.js
@@ -1,8 +1,8 @@
-toSearch = [9, 8, 4, 3, 2, 1, 5, 6, 7];
-toFind = 7;
+let toSearch = [9, 8, 4, 3, 2, 1, 5, 6, 7];
+let toFind = 7;
 
 for (i = 0; i < toSearch.length; i++) {
     if (toFind == toSearch[i]) {
-        console.log(i)
+        console.log(i);
     }
 }
